c135fa0afc159e5dae761fcd054807216c255645
ALVARLEUNG <alvar.leung@qq.com>
2019-06-27 10:44:50 +0800

    finish take-out-food

---
+++ src/main/java/Order.java
@@ -0,0 +1,20 @@
+public class Order {
+    Item item;
+    int number;
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
